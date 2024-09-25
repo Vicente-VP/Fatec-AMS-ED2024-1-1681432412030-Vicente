1b256e588b6fb430bcd571e95293f9b272a50ec2
Enzo-dorta <91228736+Enzo-dorta@users.noreply.github.com>
2024-09-25 18:41:33 +0000

    adcionando a regra de negocio de bisavô/bisavó, com cores de cada ação

--- Exercicios_aula_estruturaDados/Arvore_Genealogica/ArvoreGenealogica/arvore.py
+++ Exercicios_aula_estruturaDados/Arvore_Genealogica/ArvoreGenealogica/arvore.py
@@ -1,3 +1,8 @@
+import colorama
+from colorama import Fore, Back, Style
+
+colorama.init()
+
 tam_familia = int(input("Digite o tamanho da sua familia: "))
 
 familia = {}
@@ -8,7 +13,7 @@ for i in range (tam_familia):
     idade = int(input("digite  idade dele: "))
     familia[f'{nome}'] = idade
 
-for i in sorted(familia, key = familia.get):
+for i in sorted(familia, key = familia.get, reverse = True):
     familia_ordem[f'{i}'] = familia[i]
     
 print(familia_ordem)
@@ -17,11 +22,35 @@ lista_familia = list(familia_ordem.items())
 
 for i in range(len(lista_familia)):
     nome_now, idade_now = lista_familia[i]
+    nomeM, idadeM = lista_familia[0]
 
     for j in range(len(lista_familia)):
         if i != j:
             nome_next, idade_next = lista_familia[j]
 
+            diferenca = idade_now - idade_next
 
-    
-    
+            if diferenca > 15 and diferenca <= 50:
+                print(Fore.BLUE + nome_now + " e " + nome_next + f" podem ser pai/mãe e filho(a). diferença {diferenca}")
+
+            if diferenca < -15 and diferenca >= -50:
+                print(Fore.BLUE + nome_now + " e " + nome_next + f" podem ser pai/mãe e filho(a). diferença {diferenca}")
+
+            if diferenca <= 15 and diferenca >= -15:
+                print(Fore.YELLOW + nome_now + " e " + nome_next + f" podem ser irmãos(ãs) ou casal . diferença {diferenca}")
+
+            if diferenca < -80 or diferenca > 80:
+                print(Fore.GREEN + nome_now + " e " + nome_next + f" podem ser bisavô(vó) e bisaneto(a)")
+            
+            if diferenca < 80 and diferenca > 50:
+                print(Fore.RED + nome_now + " e " + nome_next + f" podem ser avô(ó) e neto(a)")
+
+            if diferenca < -50 and diferenca > -80:
+                print(Fore.RED + nome_now + " e " + nome_next + f" podem ser avô(ó) e neto(a)")
+
+            colorama.deinit()
+
+
+
+
+# bisavo - bisneto > 0 -> 
